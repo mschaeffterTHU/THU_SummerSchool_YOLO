85f8282e7feaab6f052d8ce6e0b08da6ac74191e
Jan Leon <Jan.gaschler@gmail.com>
2025-07-01 19:27:48 +0200

    fix: add apt upgrade

--- src/hands_on_tutorial/utils/libraries.bash
+++ src/hands_on_tutorial/utils/libraries.bash
@@ -7,6 +7,7 @@ pip install mediapipe
 pip install pygame
 
 sudo apt update
+sudo apt upgrade -y
 sudo apt install -y build-essential cmake libopenblas-dev liblapack-dev \
     libx11-dev libgtk-3-dev python3-dev python3-pip
 
